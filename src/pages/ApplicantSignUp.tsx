6bf0e57353ebb693413277ddebd678ac8f6c2b3a
1myeji <106291546+1myeji@users.noreply.github.com>
2023-03-23 00:07:07 +0900

    Refactor : validationSchema 파일 만들기

--- src/pages/ApplicantSignUp.tsx
+++ src/pages/ApplicantSignUp.tsx
@@ -1,33 +1,7 @@
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
-import * as yup from 'yup';
+import { schema } from './../utils/validationSchema';
 import axios from 'axios';
-
-const schema = yup.object({
-  email: yup
-    .string()
-    .required('이메일은 필수 입력입니다.')
-    .matches(/^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z]{2,}$/, '이메일 형식에 맞지 않습니다.'),
-  password: yup
-    .string()
-    .required('비밀번호는 필수 입력입니다.')
-    .matches(/^[a-zA-Z0-9]+$/, '영문자, 숫자를 조합하여 입력해주세요.')
-    .min(8, '비밀번호는 최소 8자리 이상 입력해 주세요.')
-    .max(15, '비밀번호는 최대 15자리로 입력해 주세요.'),
-  lastName: yup
-    .string()
-    .required('성은 필수 입력입니다.')
-    .matches(/^[a-zA-Z가-힣]+$/, '영문자, 한글을 입력해주세요.'),
-  firstName: yup
-    .string()
-    .required('이름은 필수 입력입니다.')
-    .matches(/^[a-zA-Z가-힣]+$/, '영문자, 한글을 입력해주세요.'),
-  birth: yup.string().required('생년월일은 필수 입력입니다.'),
-  phoneNumber: yup
-    .string()
-    .required('전화번호는 필수 입력입니다.')
-    .matches(/^\d{3}-\d{3,4}-\d{4}$/, '전화번호 형식에 알맞지 않습니다. -를 포함해서 입력하세요'),
-});
 
 const ApplicantSignUp = () => {
   const { register, handleSubmit, formState } = useForm<IApplicantSignUpData>({
@@ -80,7 +54,13 @@ const ApplicantSignUp = () => {
       <input id='phoneNumber' type='tel' {...register('phoneNumber')} placeholder='010-1234-5678' />
       <p style={{ margin: '10px 0', color: 'red' }}>{formState.errors.phoneNumber?.message}</p>
 
-      <button style={{ backgroundColor: formState.isValid ? 'orange' : '' }}>회원가입</button>
+      <button
+        style={{
+          backgroundColor: formState.isValid ? 'orange' : '',
+        }}
+      >
+        회원가입
+      </button>
     </form>
   );
 };
