920b8ca5b5db8444f3ea7e03a860a258d9aeb8bf
soyun <soyoon4122@naver.com>
2023-03-22 15:48:04 +0900

    Feat: global 스타일 / 폰트 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Layout from './components/layouts';
+import GlobalStyle from './style/globalStyle';
 import Home from './pages/Home';
 import Login from './pages/Login';
 
 function App() {
   return (
     <BrowserRouter>
+      <GlobalStyle />
       <Routes>
         <Route element={<Layout />}>
           <Route path='/' element={<Home />} />
