7c82c58dac85534903bfce8e6b77a498967c0206
soyun <soyoon4122@naver.com>
2023-03-24 15:38:18 +0900

    Feat: 리덕스 툴킷 셋팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
 import Layout from './components/layouts';
 import GlobalStyle from './style/globalStyle';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import ApplicantSignUp from './pages/ApplicantSignUp';
 import CompanySignUp from './pages/CompanySignUp';
+import Loading from './components/common/Loading';
 
 function App() {
   return (
     <BrowserRouter>
       <GlobalStyle />
+      <Loading />
       <Routes>
         <Route element={<Layout />}>
           <Route path='/' element={<Home />} />
