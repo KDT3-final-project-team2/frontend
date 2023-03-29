3b13804179665c985bf082754e94519cc75d33bb
soyun <soyoon4122@naver.com>
2023-03-29 18:58:53 +0900

    Feat: 지원자 통계 추가

---
+++ src/components/applicant/ApplicantsRecommend.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+type Props = {};
+
+const ApplicantsRecommend = (props: Props) => {
+  return <div>ApplicantsRecommend</div>;
+};
+
+export default ApplicantsRecommend;
--- src/pages/company/CompanyApplicant.tsx
+++ src/pages/company/CompanyApplicant.tsx
@@ -1,7 +1,81 @@
-import React from 'react';
+import { useState } from 'react';
+import styled from 'styled-components';
+import ApplicantsInfo from './../../components/applicant/ApplicantsInfo';
+import ApplicantsRecommend from './../../components/applicant/ApplicantsRecommend';
+import MailTemplate from './../../components/applicant/MailTemplate';
 
 const CompanyApplicant = () => {
-  return <div>CompanyApplicant</div>;
+  const [activeTab, setActiveTab] = useState(0);
+
+  const tabs = [
+    { index: 1, name: '지원자 정보 보관함', content: <ApplicantsInfo activeTab={activeTab} /> },
+    { index: 2, name: '맞춤 지원자 추천', content: <ApplicantsRecommend /> },
+    { index: 3, name: '메일 템플릿', content: <MailTemplate /> },
+  ];
+
+  return (
+    <ContainerInner>
+      <Title>지원자 통계</Title>
+      <TabContent>
+        <ul>
+          {tabs.map((item, idx) => (
+            <li
+              key={item.index}
+              className={activeTab === idx ? 'active' : ''}
+              onClick={() => {
+                setActiveTab(idx);
+              }}
+            >
+              {item.name}
+            </li>
+          ))}
+        </ul>
+      </TabContent>
+      <div>{tabs[activeTab].content}</div>
+    </ContainerInner>
+  );
 };
+
+const ContainerInner = styled.div`
+  padding: 30px 70px 0;
+`;
+
+const Title = styled.h1`
+  font-size: 34px;
+  margin-bottom: 30px;
+  letter-spacing: -0.5px;
+`;
+
+const TabContent = styled.div`
+  padding: 0 30px;
+  ul {
+    display: flex;
+    align-items: center;
+    gap: 50px;
+    li {
+      cursor: pointer;
+      color: var(--color-gray-500);
+      font-size: 20px;
+      font-weight: bold;
+      letter-spacing: -0.5px;
+      line-height: 50px;
+      position: relative;
+      padding: 0 10px;
+      &.active {
+        color: var(--color-primary-100);
+        :after {
+          content: '';
+          position: absolute;
+          bottom: 0;
+          left: 0;
+          width: 100%;
+          height: 5px;
+          border-radius: 50px;
+          background-color: var(--color-primary-100);
+        }
+      }
+    }
+  }
+`;
 
 export default CompanyApplicant;
