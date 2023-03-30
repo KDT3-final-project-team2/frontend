18da2de7bd6e61d694b76d4de5bb221161b6649a
soyun <soyoon4122@naver.com>
2023-03-30 19:53:44 +0900

    chart 라이브러리 적용

---
+++ src/components/company/ApplicantsInfo.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+type Props = {};
+
+const ApplicantsInfo = (activeTab: any) => {
+  return <div>ApplicantsInfo</div>;
+};
+
+export default ApplicantsInfo;
---
+++ src/components/company/MailTemplate.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+type Props = {};
+
+const MailTemplate = (props: Props) => {
+  return <div>MailTemplate</div>;
+};
+
+export default MailTemplate;
