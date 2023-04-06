7a722dc693db318c1bf750601b5dc1b5d52caab1
soyun <soyoon4122@naver.com>
2023-04-06 15:20:06 +0900

    Feat: CookiesProvider 추가

--- src/main.tsx
+++ src/main.tsx
@@ -6,17 +6,20 @@ import { PersistGate } from 'redux-persist/integration/react';
 import { store } from './store/store';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { CookiesProvider } from 'react-cookie';
 
 let persistor = persistStore(store);
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <QueryClientProvider client={queryClient}>
-        <App />
-        <ReactQueryDevtools initialIsOpen={true} />
-      </QueryClientProvider>
-    </PersistGate>
-  </Provider>,
+  <CookiesProvider>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <QueryClientProvider client={queryClient}>
+          <App />
+          <ReactQueryDevtools initialIsOpen={true} />
+        </QueryClientProvider>
+      </PersistGate>
+    </Provider>
+  </CookiesProvider>,
 );
